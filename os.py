637c1bd0ec06f642bca8e415881a90ea51870ab6
stevekevin1005 <qqwwee2006@gmail.com>
2016-06-10 02:41:59 +0800

    add mutex up

--- os.py
+++ os.py
@@ -1,72 +1,147 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
+"""Explain here what this module has to offer."""
+from __future__ import print_function
+import copy
+import random
+import sys
 import threading
 import time
-import random
-import copy
 
-exitFlag = 0
-
-class checkup (threading.Thread):  #繼承thred 檢查流程
-  def __init__(self, threadID, patientList, patientStatus):
+class CheckUp2(threading.Thread):
+  """Explain here what this class represents and some usage hints."""
+  def __init__(self, **kwargs):
     threading.Thread.__init__(self)
-    self.threadID = threadID
-    self.patientList = patientList
-    self.patientStatus = patientStatus
-  def run(self):                   #檢查中
+    self.threadID = kwargs['area_name']
+    self.patient_list = kwargs['patient_list']
+
+  def run(self):
+    """Describe logic of processing here."""
+    global mutex,patients,checkUP1Num,mutex2
+    s_k, n_k , c_c= 'status', 'name', 'checkCount'
+    busy_sleep_secs = 1
     while True:
-      if(check(self.patientList)):
+      if check(self.patient_list):
         break
-      for i in range( 0, 2, 1):
-        for status in self.patientStatus:
-          if status['name'] == self.patientList[i]['name'] and status['status'] == False and self.patientList[i]['status'] == False:
-            status['status'] = True
-            checkTime = random.randint(1,3)
-            print "%s: %s check time(%d)" % (self.patientList[i]['name'], self.threadID, checkTime)
-            time.sleep(checkTime)
-            self.patientList[i]['status'] = True
-            status['status'] = False
-            time.sleep(1)
+      for i in range(0, len(self.patient_list), 1):
+        for status in patients:
+          if status[n_k] == self.patient_list[i][n_k]:
+            mutex.acquire()
+            if status[c_c] >= checkUP1Num:
+              if not (status[s_k] or self.patient_list[i][s_k]):
+                status[c_c] = status[c_c] + 1;
+                status[s_k] = True
+                check_time = random.randint(1, 3)
+                mutex2.acquire()
+                print("%s: %s check time(%d)"
+                   "" % (self.patient_list[i][n_k],
+                      self.threadID, check_time))
+                time.sleep(check_time)
+                self.patient_list[i][s_k] = True
+                mutex2.release()
+                status[s_k] = False
+                # time.sleep(busy_sleep_secs)
+            mutex.release()
 
-#check all patients have to be ckecked
-def check(patientList):
-  checkFlag = False
-  for patient in patientList:
-    if patient['status'] == True:
-      checkFlag = True
-    else:
-      checkFlag = False
-  if checkFlag:
-    return True
-  else:
-    return False
 
-# patients init
-patients = [{
-  'name': 'Kevin',
-  'status': False
-},{
-  'name': 'Tom',
-  'status': False
-}]
+class CheckUp(threading.Thread):
+  """Explain here what this class represents and some usage hints."""
+  def __init__(self, **kwargs):
+    threading.Thread.__init__(self)
+    self.threadID = kwargs['area_name']
+    self.patient_list = kwargs['patient_list']
 
-# Outpatient init
-random.shuffle(patients)
-Cardiology = {
-  'name': 'Cardiology',
-  'patientList':  copy.deepcopy(patients),
-  'patientStatus': patients
-}
-CardiologyCheckup = checkup('Cardiology', Cardiology['patientList'], Cardiology['patientStatus'])
+  def run(self):
+    """Describe logic of processing here."""
+    global mutex,patients,mutex2
+    s_k, n_k , c_c= 'status', 'name', 'checkCount'
+    busy_sleep_secs = 1
+    while True:
+      if check(self.patient_list):
+        break
+      for i in range(0, len(self.patient_list), 1):
+        for status in patients:
+          if status[n_k] == self.patient_list[i][n_k]:
+            mutex.acquire()
+            
+            if not (status[s_k] or self.patient_list[i][s_k]):
+              status[c_c] = status[c_c] + 1;
+              status[s_k] = True
+              check_time = random.randint(1, 3)
+              mutex2.acquire()
+              print("%s: %s check time(%d)"
+                 "" % (self.patient_list[i][n_k],
+                    self.threadID, check_time))
+              time.sleep(check_time)
+              self.patient_list[i][s_k] = True
+              mutex2.release()
+              status[s_k] = False
+              time.sleep(busy_sleep_secs)
+            mutex.release()
 
-random.shuffle(patients)
-Chest = {
-  'name': 'Chest',
-  'patientList':  copy.deepcopy(patients),
-  'patientStatus': patients
-}
-ChestCheckup = checkup('Chest', Chest['patientList'], Chest['patientStatus'])
 
-ChestCheckup.start()
-CardiologyCheckup.start()
+def check(patient_list):
+  """Check all patients have to be checked."""
+  global mutex2
+  check_flag = True
+  mutex2.acquire()
+  for patient in patient_list:
+    if(patient['status'] == False):
+      check_flag = False
+  mutex2.release()
+  return check_flag
+
+
+def main():
+  """Drive the endeavour."""
+  
+
+  # Outpatient init
+  random.shuffle(patients)
+  cardiology = {
+    'area_name': 'Cardiology',
+    'patient_list': copy.deepcopy(patients)
+  }
+  cardiologyCheckup = CheckUp(**cardiology)
+
+  random.shuffle(patients)
+  chest = {
+    'area_name': 'Chest',
+    'patient_list': copy.deepcopy(patients)
+  }
+  chestCheckup = CheckUp(**chest)
+
+  random.shuffle(patients)
+  ophthalmology = {
+    'area_name': 'Ophthalmology',
+    'patient_list': copy.deepcopy(patients)
+  }
+  ophthalmologyCheckup = CheckUp2(**ophthalmology)
+
+  ophthalmologyCheckup.start()
+  chestCheckup.start()
+  cardiologyCheckup.start()
+  
+
+
+if __name__ == '__main__':
+  # patients init
+  patients = [{
+    'name': 'Kevin',
+    'status': False,
+    'checkCount': 0
+  }, {
+    'name': 'Tom',
+    'status': False,
+    'checkCount': 0
+  },{
+    'name': 'Amy',
+    'status': False,
+    'checkCount': 0
+  }]
+  #checkUp step1 Num
+  checkUP1Num = 2;
+
+  mutex = threading.Lock()
+  mutex2 = threading.Lock()
+  sys.exit(main())
